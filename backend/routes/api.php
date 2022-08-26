2b36b3b7ce5f30dec6e9d4a18e6a1d2ca3e760c1
joseluisposada <jlposada@misena.edu.co>
2022-08-26 11:26:00 -0500

    ultimo

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BootcampController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+ //   return $request->user();
+//});
+
+
+//La clase Route solo se encarga de rutas
+//metodo apiResource: permite crear rutas de cambio de estado para una entidad
+
+Route::apiResource('bootcamps', BootcampController::class);
